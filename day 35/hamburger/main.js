68c60c63aecd776e36f037c0e8e6fe9c19921547
shuajbi1 <100381835+shuajbi1@users.noreply.github.com>
2025-09-27 05:49:32 -0700

    day 35

---
+++ day 35/hamburger/main.js
@@ -0,0 +1,28 @@
+function openMenu(){
+    var menu = document.getElementById("links");
+    if(menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+function search(){
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("mylist");
+    li = ul.getElementsByTagName("li");
+    
+    for (i = 0; i < li.length; i++) {
+        a = li[i];
+       txtValue = a.textContent || a.innerText;
+         
+        if (txtValue.toUpperCase().indexOf(filter)>-1){ 
+            li[i].style.display = "block";
+        } else {   li[i].style.display = "none";
+        }
+} 
+    }
+ 
+    
